788f45b3648b9bcb312312bda17348b30cdc7390
Geordan Rosario <geordan@doselab.com>
2012-11-29 00:47:48 -0800

    Proof of concept: named pilot uniqueness checking.

--- main.coffee
+++ main.coffee
@@ -7,21 +7,36 @@ class exportObj.SquadBuilder
     # Tracks which pilots are in use.
     constructor: (args) ->
         @faction = args.faction
+        @pilots = []
 
-    getPilotNames: () ->
-        # Returns list of pilot names for this faction.
+    getAvailablePilots: () ->
+        # Returns list of available pilot names for this faction.
         ships = (ship_name for ship_name, ship_data of exportObj.ships when ship_data.faction == @faction)
-        pilots = (pilot_name for pilot_name, pilot_data of exportObj.pilots when pilot_data.ship in ships).sort()
+        (pilot_name for pilot_name, pilot_data of exportObj.pilots when pilot_data.ship in ships and (not pilot_data.unique? or pilot_name not in @pilots)).sort()
+
+    addPilot: (pilot_name) ->
+        pilot = new Pilot(pilot_name)
+        # Check faction
+        if pilot.data.faction != @faction
+            throw "Pilot is of the wrong faction"
+        # Check for uniqueness
+        if pilot.data.unique? and pilot.name in @pilots
+            throw "Pilot is unique and cannot be used multiple times"
 
 class Pilot
     # Represents a pilot with upgrades.
     constructor: (pilot_name) ->
-        @pilot = exportObj.pilots[pilot_name]
-        @ship = exportObj.ships[@pilot.ship]
-        @active_upgrades = {}
+        @name = pilot_name
+        @data = exportObj.pilots[pilot_name]
+        @ship = exportObj.ships[@data.ship]
+        @active_upgrades = []
 
     addUpgrade: (upgrade) ->
-        @active_upgrades[upgrade] = true
+        # Check if there's an available slot and drop it in if so
+        # Possibly overwrite existing slot if no free ones available
 
     removeUpgrade: (upgrade) ->
-        @active_upgrades[upgrade] ?= undefined
+        # Empty slot
+
+    getUpgradesForSlot: (slot) ->
+        (upgrade_name for upgrade_name, upgrade_data of exportObj.upgrades when upgrade_data == slot).sort()
--- main.js
+++ main.js
@@ -8,10 +8,11 @@
 
     function SquadBuilder(args) {
       this.faction = args.faction;
+      this.pilots = [];
     }
 
-    SquadBuilder.prototype.getPilotNames = function() {
-      var pilot_data, pilot_name, pilots, ship_data, ship_name, ships;
+    SquadBuilder.prototype.getAvailablePilots = function() {
+      var pilot_data, pilot_name, ship_data, ship_name, ships;
       ships = (function() {
         var _ref, _results;
         _ref = exportObj.ships;
@@ -22,18 +23,29 @@
         }
         return _results;
       }).call(this);
-      return pilots = ((function() {
+      return ((function() {
         var _ref, _ref2, _results;
         _ref = exportObj.pilots;
         _results = [];
         for (pilot_name in _ref) {
           pilot_data = _ref[pilot_name];
-          if (_ref2 = pilot_data.ship, __indexOf.call(ships, _ref2) >= 0) {
+          if ((_ref2 = pilot_data.ship, __indexOf.call(ships, _ref2) >= 0) && (!(pilot_data.unique != null) || __indexOf.call(this.pilots, pilot_name) < 0)) {
             _results.push(pilot_name);
           }
         }
         return _results;
-      })()).sort();
+      }).call(this)).sort();
+    };
+
+    SquadBuilder.prototype.addPilot = function(pilot_name) {
+      var pilot, _ref;
+      pilot = new Pilot(pilot_name);
+      if (pilot.data.faction !== this.faction) {
+        throw "Pilot is of the wrong faction";
+      }
+      if ((pilot.data.unique != null) && (_ref = pilot.name, __indexOf.call(this.pilots, _ref) >= 0)) {
+        throw "Pilot is unique and cannot be used multiple times";
+      }
     };
 
     return SquadBuilder;
@@ -43,18 +55,28 @@
   Pilot = (function() {
 
     function Pilot(pilot_name) {
-      this.pilot = exportObj.pilots[pilot_name];
-      this.ship = exportObj.ships[this.pilot.ship];
-      this.active_upgrades = {};
+      this.name = pilot_name;
+      this.data = exportObj.pilots[pilot_name];
+      this.ship = exportObj.ships[this.data.ship];
+      this.active_upgrades = [];
     }
 
-    Pilot.prototype.addUpgrade = function(upgrade) {
-      return this.active_upgrades[upgrade] = true;
-    };
+    Pilot.prototype.addUpgrade = function(upgrade) {};
 
-    Pilot.prototype.removeUpgrade = function(upgrade) {
-      var _base, _ref;
-      return (_ref = (_base = this.active_upgrades)[upgrade]) != null ? _ref : _base[upgrade] = void 0;
+    Pilot.prototype.removeUpgrade = function(upgrade) {};
+
+    Pilot.prototype.getUpgradesForSlot = function(slot) {
+      var upgrade_data, upgrade_name;
+      return ((function() {
+        var _ref, _results;
+        _ref = exportObj.upgrades;
+        _results = [];
+        for (upgrade_name in _ref) {
+          upgrade_data = _ref[upgrade_name];
+          if (upgrade_data === slot) _results.push(upgrade_name);
+        }
+        return _results;
+      })()).sort();
     };
 
     return Pilot;
