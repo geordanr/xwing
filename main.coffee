e1a6d9a3083611f34b54a9847c669b82b7ba3e1f
Geordan Rosario <geordan@doselab.com>
2012-11-30 22:47:35 -0800

    More screwing around.

--- main.coffee
+++ main.coffee
@@ -2,41 +2,125 @@ exportObj = exports ? this
 
 # Assumes that cards.js has been loaded already and data is on exportObj.
 
+exportObj.sortHelper = (a, b) ->
+    a = a.name.replace(/[^a-z0-9]/ig, '')
+    b = b.name.replace(/[^a-z0-9]/ig, '')
+    if a == b
+        0
+    else
+        if a > b then 1 else -1
+
 class exportObj.SquadBuilder
     # Superclass for faction builders.
     # Tracks which pilots are in use.
+    #
+    # Usage:
+    #
+    #   sb = new SquadBuilder
+    #       container: '#empire-builder'
+    #       faction: "Galactic Empire"
     constructor: (args) ->
+        @container = $(args.container)
         @faction = args.faction
+        @rows = []
         @pilots = []
+
+        # Add initial row
+        # TODO deserialize a previous configuration
+        @rows.push new PilotRow this
+        @rows.push new PilotRow this
+
+        $(window).bind 'xwing:pilotChanged', (e, triggering_row) =>
+            @pilots = (row.name for row in @rows when row.name? and row.name != '')
+            for row in @rows
+                if row != triggering_row
+                    row.update()
 
     getAvailablePilots: () ->
         # Returns list of available pilot names for this faction.
         ships = (ship_name for ship_name, ship_data of exportObj.ships when ship_data.faction == @faction)
-        (pilot_name for pilot_name, pilot_data of exportObj.pilots when pilot_data.ship in ships and (not pilot_data.unique? or pilot_name not in @pilots)).sort()
+        ({name: pilot_name, points: pilot_data.points} for pilot_name, pilot_data of exportObj.pilots when pilot_data.ship in ships and (not pilot_data.unique? or pilot_name not in @pilots))
 
-    addPilot: (pilot_name) ->
-        pilot = new Pilot(pilot_name)
-        # Check faction
-        if pilot.data.faction != @faction
-            throw "Pilot is of the wrong faction"
-        # Check for uniqueness
-        if pilot.data.unique? and pilot.name in @pilots
-            throw "Pilot is unique and cannot be used multiple times"
-
-class Pilot
-    # Represents a pilot with upgrades.
-    constructor: (pilot_name) ->
-        @name = pilot_name
-        @data = exportObj.pilots[pilot_name]
-        @ship = exportObj.ships[@data.ship]
+class PilotRow
+    # Represents a pilot row in the UI.
+    constructor: (builder) ->
+        @builder = builder
+        @name = null
+        @pilot = null
+        @ship = null
         @active_upgrades = []
 
-    addUpgrade: (upgrade) ->
-        # Check if there's an available slot and drop it in if so
-        # Possibly overwrite existing slot if no free ones available
+        # set up UI elements
+        @row = $(document.createElement 'DIV')
+        @row.addClass 'row'
+        @builder.container.append @row
 
-    removeUpgrade: (upgrade) ->
-        # Empty slot
+        @pilot_cell = $(document.createElement 'DIV')
+        @pilot_cell.addClass 'four columns'
+        @row.append @pilot_cell
 
-    getUpgradesForSlot: (slot) ->
-        (upgrade_name for upgrade_name, upgrade_data of exportObj.upgrades when upgrade_data == slot).sort()
+        @pilot_selector = $(document.createElement 'SELECT')
+        @pilot_selector.addClass 'pilot'
+        @pilot_selector.attr 'data-placeholder', 'Select a pilot'
+        @pilot_selector.change (e) =>
+            @active_upgrades = []
+            @name = @pilot_selector.val()
+            if @name == ''
+                @pilot = null
+                @ship = null
+                # Clear upgrades
+                @upgrade_cell.text 'Select a pilot to view upgrades'
+            else
+                @upgrade_cell.text ''
+                @pilot = exportObj.pilots[@name]
+                @ship = exportObj.ships[@pilot.ship]
+                #  Set upgrade selectors
+                for slot in @pilot.slots
+                    @active_upgrades.push null
+                    @addUpgradeSelector slot
+            $(window).trigger 'xwing:pilotChanged', this
+        @pilot_cell.append @pilot_selector
+        @pilot_selector.chosen()
+
+        @upgrade_cell = $(document.createElement 'DIV')
+        @upgrade_cell.addClass 'eight columns upgrades'
+        @row.append @upgrade_cell
+
+        @update()
+        @pilot_selector.change()
+
+    update: () ->
+        # Update the contents of the selector when another pilot is selected
+        @pilot_selector.text ''
+        @pilot_selector.append document.createElement 'OPTION'
+        available_pilots = @builder.getAvailablePilots()
+        # Re-add our selected pilot if there is one
+        if @pilot
+            available_pilots.push
+                name: @name
+                points: @pilot.points
+        available_pilots.sort exportObj.sortHelper
+        for pilot in available_pilots
+            option = $(document.createElement 'OPTION')
+            option.text "#{pilot.name} (#{pilot.points})"
+            option.val pilot.name
+            @pilot_selector.append option
+        @pilot_selector.val @name
+        @pilot_selector.trigger 'liszt:updated'
+
+    addUpgradeSelector: (slot, slot_index) ->
+        upgrades = ({name: upgrade_name, points: upgrade_data.points} for upgrade_name, upgrade_data of exportObj.upgrades when upgrade_data.slot == slot).sort exportObj.sortHelper
+
+        selector = $(document.createElement 'SELECT')
+        selector.addClass 'upgrade'
+        selector.append document.createElement 'OPTION'
+        selector.attr 'data-placeholder', "Select #{slot} Upgrade"
+        for upgrade in upgrades
+            option = $(document.createElement 'OPTION')
+            option.text "#{upgrade.name} (#{upgrade.points})"
+            option.val upgrade
+            selector.append option
+        selector.change (e) =>
+            @active_upgrades[slot_index] = selector.val()
+        @upgrade_cell.append selector
+        selector.chosen()
--- main.js
+++ main.js
@@ -1,18 +1,61 @@
 (function() {
-  var Pilot, exportObj,
+  var PilotRow, exportObj,
     __indexOf = Array.prototype.indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
 
   exportObj = typeof exports !== "undefined" && exports !== null ? exports : this;
 
+  exportObj.sortHelper = function(a, b) {
+    a = a.name.replace(/[^a-z0-9]/ig, '');
+    b = b.name.replace(/[^a-z0-9]/ig, '');
+    if (a === b) {
+      return 0;
+    } else {
+      if (a > b) {
+        return 1;
+      } else {
+        return -1;
+      }
+    }
+  };
+
   exportObj.SquadBuilder = (function() {
 
     function SquadBuilder(args) {
+      var _this = this;
+      this.container = $(args.container);
       this.faction = args.faction;
+      this.rows = [];
       this.pilots = [];
+      this.rows.push(new PilotRow(this));
+      this.rows.push(new PilotRow(this));
+      $(window).bind('xwing:pilotChanged', function(e, triggering_row) {
+        var row, _i, _len, _ref, _results;
+        _this.pilots = (function() {
+          var _i, _len, _ref, _results;
+          _ref = this.rows;
+          _results = [];
+          for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+            row = _ref[_i];
+            if ((row.name != null) && row.name !== '') _results.push(row.name);
+          }
+          return _results;
+        }).call(_this);
+        _ref = _this.rows;
+        _results = [];
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          row = _ref[_i];
+          if (row !== triggering_row) {
+            _results.push(row.update());
+          } else {
+            _results.push(void 0);
+          }
+        }
+        return _results;
+      });
     }
 
     SquadBuilder.prototype.getAvailablePilots = function() {
-      var pilot_data, pilot_name, ship_data, ship_name, ships;
+      var pilot_data, pilot_name, ship_data, ship_name, ships, _ref, _ref2, _results;
       ships = (function() {
         var _ref, _results;
         _ref = exportObj.ships;
@@ -23,63 +66,132 @@
         }
         return _results;
       }).call(this);
-      return ((function() {
-        var _ref, _ref2, _results;
-        _ref = exportObj.pilots;
-        _results = [];
-        for (pilot_name in _ref) {
-          pilot_data = _ref[pilot_name];
-          if ((_ref2 = pilot_data.ship, __indexOf.call(ships, _ref2) >= 0) && (!(pilot_data.unique != null) || __indexOf.call(this.pilots, pilot_name) < 0)) {
-            _results.push(pilot_name);
-          }
+      _ref = exportObj.pilots;
+      _results = [];
+      for (pilot_name in _ref) {
+        pilot_data = _ref[pilot_name];
+        if ((_ref2 = pilot_data.ship, __indexOf.call(ships, _ref2) >= 0) && (!(pilot_data.unique != null) || __indexOf.call(this.pilots, pilot_name) < 0)) {
+          _results.push({
+            name: pilot_name,
+            points: pilot_data.points
+          });
         }
-        return _results;
-      }).call(this)).sort();
-    };
-
-    SquadBuilder.prototype.addPilot = function(pilot_name) {
-      var pilot, _ref;
-      pilot = new Pilot(pilot_name);
-      if (pilot.data.faction !== this.faction) {
-        throw "Pilot is of the wrong faction";
       }
-      if ((pilot.data.unique != null) && (_ref = pilot.name, __indexOf.call(this.pilots, _ref) >= 0)) {
-        throw "Pilot is unique and cannot be used multiple times";
-      }
+      return _results;
     };
 
     return SquadBuilder;
 
   })();
 
-  Pilot = (function() {
+  PilotRow = (function() {
 
-    function Pilot(pilot_name) {
-      this.name = pilot_name;
-      this.data = exportObj.pilots[pilot_name];
-      this.ship = exportObj.ships[this.data.ship];
+    function PilotRow(builder) {
+      var _this = this;
+      this.builder = builder;
+      this.name = null;
+      this.pilot = null;
+      this.ship = null;
       this.active_upgrades = [];
+      this.row = $(document.createElement('DIV'));
+      this.row.addClass('row');
+      this.builder.container.append(this.row);
+      this.pilot_cell = $(document.createElement('DIV'));
+      this.pilot_cell.addClass('four columns');
+      this.row.append(this.pilot_cell);
+      this.pilot_selector = $(document.createElement('SELECT'));
+      this.pilot_selector.addClass('pilot');
+      this.pilot_selector.attr('data-placeholder', 'Select a pilot');
+      this.pilot_selector.change(function(e) {
+        var slot, _i, _len, _ref;
+        _this.active_upgrades = [];
+        _this.name = _this.pilot_selector.val();
+        if (_this.name === '') {
+          _this.pilot = null;
+          _this.ship = null;
+          _this.upgrade_cell.text('Select a pilot to view upgrades');
+        } else {
+          _this.upgrade_cell.text('');
+          _this.pilot = exportObj.pilots[_this.name];
+          _this.ship = exportObj.ships[_this.pilot.ship];
+          _ref = _this.pilot.slots;
+          for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+            slot = _ref[_i];
+            _this.active_upgrades.push(null);
+            _this.addUpgradeSelector(slot);
+          }
+        }
+        return $(window).trigger('xwing:pilotChanged', _this);
+      });
+      this.pilot_cell.append(this.pilot_selector);
+      this.pilot_selector.chosen();
+      this.upgrade_cell = $(document.createElement('DIV'));
+      this.upgrade_cell.addClass('eight columns upgrades');
+      this.row.append(this.upgrade_cell);
+      this.update();
+      this.pilot_selector.change();
     }
 
-    Pilot.prototype.addUpgrade = function(upgrade) {};
+    PilotRow.prototype.update = function() {
+      var available_pilots, option, pilot, _i, _len;
+      this.pilot_selector.text('');
+      this.pilot_selector.append(document.createElement('OPTION'));
+      available_pilots = this.builder.getAvailablePilots();
+      if (this.pilot) {
+        available_pilots.push({
+          name: this.name,
+          points: this.pilot.points
+        });
+      }
+      available_pilots.sort(exportObj.sortHelper);
+      for (_i = 0, _len = available_pilots.length; _i < _len; _i++) {
+        pilot = available_pilots[_i];
+        option = $(document.createElement('OPTION'));
+        option.text("" + pilot.name + " (" + pilot.points + ")");
+        option.val(pilot.name);
+        this.pilot_selector.append(option);
+      }
+      this.pilot_selector.val(this.name);
+      return this.pilot_selector.trigger('liszt:updated');
+    };
 
-    Pilot.prototype.removeUpgrade = function(upgrade) {};
-
-    Pilot.prototype.getUpgradesForSlot = function(slot) {
-      var upgrade_data, upgrade_name;
-      return ((function() {
+    PilotRow.prototype.addUpgradeSelector = function(slot, slot_index) {
+      var option, selector, upgrade, upgrade_data, upgrade_name, upgrades, _i, _len,
+        _this = this;
+      upgrades = ((function() {
         var _ref, _results;
         _ref = exportObj.upgrades;
         _results = [];
         for (upgrade_name in _ref) {
           upgrade_data = _ref[upgrade_name];
-          if (upgrade_data === slot) _results.push(upgrade_name);
+          if (upgrade_data.slot === slot) {
+            _results.push({
+              name: upgrade_name,
+              points: upgrade_data.points
+            });
+          }
         }
         return _results;
-      })()).sort();
+      })()).sort(exportObj.sortHelper);
+      selector = $(document.createElement('SELECT'));
+      selector.addClass('upgrade');
+      selector.append(document.createElement('OPTION'));
+      selector.attr('data-placeholder', "Select " + slot + " Upgrade");
+      for (_i = 0, _len = upgrades.length; _i < _len; _i++) {
+        upgrade = upgrades[_i];
+        option = $(document.createElement('OPTION'));
+        option.text("" + upgrade.name + " (" + upgrade.points + ")");
+        option.val(upgrade);
+        selector.append(option);
+      }
+      selector.change(function(e) {
+        return _this.active_upgrades[slot_index] = selector.val();
+      });
+      this.upgrade_cell.append(selector);
+      return selector.chosen();
     };
 
-    return Pilot;
+    return PilotRow;
 
   })();
 
