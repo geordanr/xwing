8fa7ab61fb4c5320e941af962b3b7f3b65d4a4f4
Blackbot <julian.schilling@tu-dortmund.de>
2018-09-25 12:57:07 +0200

    Linked Actions now use the proper symbol

--- translation_helper/translate.py
+++ translation_helper/translate.py
@@ -154,14 +154,14 @@ for card_en in cards_en:
             actions += ("<r>%s</r>" if action["base_action_side_effect"] == "stress" else "%s") % actions_by_id[
                 action["base_action_id"]]
             # if the action can be linked into another action we need to process that one as well
+            linked = "&nbsp;<i class=\"xwing-miniatures-font xwing-miniatures-font-linked\"></i>"
+            linked_stress = "&nbsp;<i class=\"xwing-miniatures-font xwing-miniatures-font-linked red\"></i>"
             if action["related_action_id"]:
-                actions += (" > <r>%s</r>" if action["related_action_side_effect"] == "stress" else " > %s") % \
-                           actions_by_id[
-                               action["base_action_id"]
-                           ]
-        card_de["ability_text"] = "<i>" + phrase_translations["adds"] % actions + "</i>" + "%LINEBREAK%" + \
-                                  card_de[
-                                      "ability_text"]
+                actions += (linked_stress + "&nbsp;<r>%s</r>" if action["related_action_side_effect"] == "stress"
+                            else linked + "&nbsp;'%s") % actions_by_id[action["related_action_id"]]
+        card_de["ability_text"] = "<i>" + phrase_translations["adds"] % actions + "</i>%LINEBREAK%" + \
+                                  card_de["ability_text"]
+
     # check for double names
     if card_en["name"] in upgrade_name_list:
         manual_stuff += ('Found upgrade name multiple times: %s\n'%card_en["name"])
